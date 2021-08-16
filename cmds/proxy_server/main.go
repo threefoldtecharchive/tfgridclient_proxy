2cca9fb5362c5d2a98ae40f257b8b0004fc832e3
Waleed <waleed.hammam@gmail.com>
2021-08-16 10:57:29 +0200

    restruct files

--- cmds/proxy_server/main.go
+++ cmds/proxy_server/main.go
@@ -12,7 +12,7 @@ import (
 	"github.com/pkg/errors"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
-	"github.com/waleedhammam/graphql-server/pkg/explorer"
+	"github.com/waleedhammam/graphql-server/explorer"
 )
 
 type flags struct {
