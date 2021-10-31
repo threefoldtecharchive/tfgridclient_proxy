11324596931abb44375906e7cd600d16aaeea54d
Waleed <waleed.hammam@gmail.com>
2021-10-31 19:07:21 +0200

    refactor: add new field to nodes to show the node state

--- internal/explorer/models.go
+++ internal/explorer/models.go
@@ -36,12 +36,12 @@ type nodeTwinID struct {
 	TwinID uint32 `json:"twinId"`
 }
 
-// NodeData is the NodeData to unmarshal json in it
+// NodeData is having nodeTwinID to unmarshal json in it
 type nodeData struct {
 	NodeResult []nodeTwinID `json:"nodes"`
 }
 
-// NodeResult is the NodeData  to unmarshal json in it
+// NodeResult is the NodeData to unmarshal nodeTwinID json in it
 type nodeResult struct {
 	Data nodeData `json:"data"`
 }
@@ -57,4 +57,58 @@ type NodeInfo struct {
 	Capacity   capacityResult `json:"capacity"`
 	DMI        dmi.DMI        `json:"dmi"`
 	Hypervisor string         `json:"hypervisor"`
+}
+
+type publicConfig struct {
+	Domain string `json:"domain"`
+	Gw4    string `json:"gw4"`
+	Gw6    string `json:"gw6"`
+	Ipv4   string `json:"ipv4"`
+	Ipv6   string `json:"ipv6"`
+}
+
+// Node is a struct holding the data for a node for the nodes view
+type node struct {
+	Version         int          `json:"version"`
+	ID              string       `json:"id"`
+	NodeID          int          `json:"nodeId"`
+	FarmID          int          `json:"farmId"`
+	TwinID          int          `json:"twinId"`
+	Country         string       `json:"country"`
+	GridVersion     int          `json:"gridVersion"`
+	City            string       `json:"city"`
+	Uptime          int64        `json:"uptime"`
+	Created         int64        `json:"created"`
+	FarmingPolicyID int          `json:"farmingPolicyId"`
+	UpdatedAt       string       `json:"updatedAt"`
+	Cru             string       `json:"cru"`
+	Mru             string       `json:"mru"`
+	Sru             string       `json:"sru"`
+	Hru             string       `json:"hru"`
+	PublicConfig    publicConfig `json:"publicConfig"`
+	State           string       `json:"state"` // added node state field for up or down
+}
+
+// Nodes is struct for the whole nodes view
+type nodes struct {
+	Data []node `json:"nodes"`
+}
+
+// NodeResponseStruct is struct for the whole nodes view
+type nodesResponse struct {
+	Nodes nodes `json:"data"`
+}
+
+type nodeID struct {
+	NodeID uint32 `json:"nodeId"`
+}
+
+// nodeIdData is the nodeIdData to unmarshal json in it
+type nodeIDData struct {
+	NodeResult []nodeID `json:"nodes"`
+}
+
+// nodeIdResult is the nodeIdResult  to unmarshal json in it
+type nodeIDResult struct {
+	Data nodeIDData `json:"data"`
 }
