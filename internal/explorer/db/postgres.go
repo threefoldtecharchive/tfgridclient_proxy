b1f2c87720c136e1169fdb41e6ffe55aad29792a
mariobassem <mariobassem12@gmail.com>
2022-08-08 16:31:59 +0200

    refactor#1

--- internal/explorer/db/postgres.go
+++ internal/explorer/db/postgres.go
@@ -49,7 +49,7 @@ const (
 		COALESCE(node_resources_total.mru, 0) as total_mru,
 		COALESCE(node_resources_total.hru, 0) as total_hru,
 		COALESCE(node_resources_total.sru, 0) as total_sru,
-		coalesce(count(distinct state), 0) as states
+		COALESCE(COUNT(DISTINCT state), 0) as states
 	FROM contract_resources
 	JOIN node_contract as node_contract
 	ON node_contract.resources_used_id = contract_resources.id AND node_contract.state IN ('Created', 'GracePeriod')
@@ -59,7 +59,7 @@ const (
 	ON node_resources_total.node_id = node.id
 	GROUP BY node.node_id, node_resources_total.mru, node_resources_total.sru, node_resources_total.hru, node_resources_total.cru;
 
-	DROP FUNCTION node_resources(query_node_id INTEGER);
+	DROP FUNCTION IF EXISTS node_resources(query_node_id INTEGER);
 	CREATE OR REPLACE function node_resources(query_node_id INTEGER)
 	returns table (node_id INTEGER, used_cru NUMERIC, used_mru NUMERIC, used_hru NUMERIC, used_sru NUMERIC, free_mru NUMERIC, free_hru NUMERIC, free_sru NUMERIC, total_cru NUMERIC, total_mru NUMERIC, total_hru NUMERIC, total_sru NUMERIC, states BIGINT)
 	as
