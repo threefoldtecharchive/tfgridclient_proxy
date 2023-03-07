a9cfbb55bbbd514b4f9fe7debe39b966ea175822
A-Harby <harbya@incubaid.com>
2023-03-07 11:43:48 +0200

    Adding network env var

--- tests/schema_test/test_schema.py
+++ tests/schema_test/test_schema.py
@@ -1,8 +1,16 @@
+import os
 import pytest
 import schemathesis
 from schemathesis.checks import not_a_server_error, status_code_conformance, content_type_conformance, response_schema_conformance, response_headers_conformance
 
-schema = schemathesis.from_path("docs/swagger.json", base_url='https://gridproxy.dev.grid.tf')
+network = os.environ['NETWORK']
+if network == 'main':
+    url = 'https://gridproxy.grid.tf'
+else:
+    url = 'https://gridproxy.' + network + '.grid.tf'
+    
+schema = schemathesis.from_path("docs/swagger.json", base_url = url)
+
 
 @pytest.mark.parametrize("check", [not_a_server_error, status_code_conformance, content_type_conformance, response_schema_conformance, response_headers_conformance])
 @schema.parametrize()
